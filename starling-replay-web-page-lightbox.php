f3d97354c065752504962253d2719f0fae68b053
Yurko <39072689+YurkoWasHere@users.noreply.github.com>
2023-11-14 18:12:39 -0500

    Update starling-replay-web-page-lightbox.php

--- starling-replay-web-page-lightbox.php
+++ starling-replay-web-page-lightbox.php
@@ -23,8 +23,8 @@ function add_script_to_head_starling_lightbox() {
     $plugin_dir_url = plugins_url('', __FILE__);
 //    wp_enqueue_script('custom-script', get_template_directory_uri() . '/custom-script.js', array(), '1.0', false);
     wp_enqueue_script('starling-replay-ui', 'https://cdn.jsdelivr.net/npm/replaywebpage@1.8.13/ui.js', array(), '1.0', false);
-    wp_enqueue_script('starling-replay-lightbox-ui', plugins_url('', __FILE__) . '/index-8b4e77ff.js', array(), '1.0', false);
-    wp_register_style('starling-lightbox-css', plugins_url('', __FILE__) . '/index-753408e7.css', array(), '1.0', 'all');
+    wp_enqueue_script('starling-replay-lightbox-ui', plugins_url('', __FILE__) . '/index-96c988fa.js', array(), '1.0', false);
+    wp_register_style('starling-lightbox-css', plugins_url('', __FILE__) . '/index-31ef780a.css', array(), '1.0', 'all');
     wp_enqueue_style('starling-lightbox-css');
    
 }
